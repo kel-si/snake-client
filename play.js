e50eda4dff3e4c6e05755a0cec03fba421d68cdc
Kelsi Olstad <kelsiolstad@gmail.com>
2022-02-16 16:29:04 -0700

    added quit

--- play.js
+++ play.js
@@ -3,6 +3,28 @@ const net = require("net");
 const connect = require("./client")
 
 
-
 console.log("Connecting ...");
 connect();
+
+
+//stdin object that is returned will allow us to listen for keyboard input and react to it
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  //function for controlling responses to user input
+  const handleUserInput = function() {
+    stdin.on("data", (key) => {
+      if (key === '\u0003') {
+        process.exit();
+      }
+    });
+  };
+  stdin.on("data", handleUserInput);
+  
+  return stdin;
+};
+
+setupInput();
